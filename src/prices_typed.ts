ea00bc7918b199447c9481776b4347e67b6e6fba
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 10:26:11 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -60,9 +60,9 @@ const createApp = (database: Database) => {
   };
 
   const calculateCostForDayTicket = (age: number | undefined, date: Date | undefined, plainDate: Temporal.PlainDate | undefined, baseCost: number) => {
-    let plainReduction = calculatePlainReduction(plainDate);
+    let reduction = calculatePlainReduction(plainDate);
     if (age === undefined) {
-      return Math.ceil(baseCost * (1 - plainReduction / 100));
+      return Math.ceil(baseCost * (1 - reduction / 100));
     }
     if (age < 6) {
       return 0;
@@ -71,9 +71,9 @@ const createApp = (database: Database) => {
       return Math.ceil(baseCost * 0.7);
     }
     if (age > 64) {
-      return Math.ceil(baseCost * 0.75 * (1 - plainReduction / 100));
+      return Math.ceil(baseCost * 0.75 * (1 - reduction / 100));
     }
-    return Math.ceil(baseCost * (1 - plainReduction / 100));
+    return Math.ceil(baseCost * (1 - reduction / 100));
   };
 
   const calculateReduction = (date: Date | undefined) => {
