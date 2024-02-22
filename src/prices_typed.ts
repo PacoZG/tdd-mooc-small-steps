92fac32cd5a257f5cf8ef3943105830d1668ac9d
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 10:19:11 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -87,11 +87,13 @@ const createApp = (database: Database) => {
   };
 
   const calculatePlainReduction = (plainDate: Temporal.PlainDate | undefined) => {
-    if (plainDate) {
+    if (plainDate && isPlainMonday(plainDate)) {
       return 35;
     }
     return 0;
   };
+
+  const isPlainMonday = (date: Temporal.PlainDate) => date.dayOfWeek === 1;
 
   const isMonday = (date: Date) => date.getDay() === 1;
 
