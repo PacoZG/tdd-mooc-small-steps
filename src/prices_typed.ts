f95e074f23cc251da0966cea7ed9067a4546e42d
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 10:09:48 +0200

    Change typescript code style to force double quotations

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -1,7 +1,7 @@
-import './polyfills';
-import express from 'express';
-import { Database } from './database';
-import { Temporal } from '@js-temporal/polyfill';
+import "./polyfills";
+import express from "express";
+import { Database } from "./database";
+import { Temporal } from "@js-temporal/polyfill";
 
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
@@ -9,14 +9,14 @@ import { Temporal } from '@js-temporal/polyfill';
 const createApp = (database: Database) => {
   const app = express();
 
-  app.put('/prices', (req, res) => {
+  app.put("/prices", (req, res) => {
     const type = req.query.type as string;
     const cost = parseInt(req.query.cost as string);
     database.setBasePrice(type, cost);
     res.json();
   });
 
-  app.get('/prices', (req, res) => {
+  app.get("/prices", (req, res) => {
     const age = req.query.age ? parseInt(req.query.age as string) : undefined;
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
@@ -32,7 +32,7 @@ const createApp = (database: Database) => {
       return new Date(dateString);
     }
   };
-
+  
   const parsePlainDate = (dateString: string | undefined): Temporal.PlainDate | undefined => {
     if (dateString) {
       return Temporal.PlainDate.from(dateString);
@@ -40,7 +40,7 @@ const createApp = (database: Database) => {
   };
 
   const calculateCost = (age: number | undefined, type: string, date: Date | undefined, baseCost: number) => {
-    if (type === 'night') {
+    if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
       return calculateCostForDayTicket(age, date, baseCost);
