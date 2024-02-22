daa1434704c5b32efeecd201d852272e90b143fb
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 08:24:32 +0200

    Increase MAX_CHANGES to 4 to meet IDE demands

--- tcr.mjs
+++ tcr.mjs
@@ -1,44 +1,49 @@
-import { execSync } from 'child_process';
-import chokidar from 'chokidar';
+import { execSync } from "child_process";
+import chokidar from "chokidar";
 
 function logCommand(command) {
-  console.log('\n> ' + command);
+  console.log("\n> " + command);
   return command;
 }
 
 function test() {
-  execSync(logCommand('npm run test'), {
-    stdio: 'inherit',
-    env: { ...process.env, MAX_CHANGES: process.env.MAX_CHANGES || '2' },
+  execSync(logCommand("npm run test"), {
+    stdio: "inherit",
+    env: {
+      ...process.env,
+      MAX_CHANGES: process.env.MAX_CHANGES || "4"
+    }
   });
 }
 
 function commit() {
-  console.log('Tests passed -> Commit changes');
-  execSync(logCommand('git commit --all --message="tcr: tests pass"'), {
-    stdio: 'inherit',
+  console.log("Tests passed -> Commit changes");
+  execSync(logCommand("git commit --all --message=\"tcr: tests pass\""), {
+    stdio: "inherit"
   });
 }
 
 function revert() {
-  console.log('Test failed -> Revert changes');
-  execSync(logCommand('git reset --hard'), { stdio: 'inherit' });
+  console.log("Test failed -> Revert changes");
+  execSync(logCommand("git reset --hard"), { stdio: "inherit" });
 }
 
 try {
   test();
-} catch (e) {
-  console.log('TCR cancelled. Tests need to start green.');
+}
+catch (e) {
+  console.log("TCR cancelled. Tests need to start green.");
   process.exit(1);
 }
 
-chokidar.watch('src').on('all', (_event, _path) => {
-  const changes = execSync('git diff --numstat', { encoding: 'utf8' });
-  if (changes !== '') {
+chokidar.watch("src").on("all", (_event, _path) => {
+  const changes = execSync("git diff --numstat", { encoding: "utf8" });
+  if (changes !== "") {
     try {
       test();
       commit();
-    } catch (e) {
+    }
+    catch (e) {
       revert();
     }
   }
