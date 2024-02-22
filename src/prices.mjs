f900a615f074dc0a0c0b74c2daeb20501c43fe96
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 09:18:28 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -57,10 +57,7 @@ const createApp = database => {
   };
 
   const calculateCostForDayTicket = (age, date, baseCost, plainDate) => {
-    let reduction = calculateReduction(date);
     let otherReduction = calculateOtherReduction(plainDate);
-    console.log('REDUCTION: ', reduction);
-    console.log('OTHER REDUCTION: ' + otherReduction);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - otherReduction / 100));
     }
