a017ce420656451ad649d9637983fc80328e79df
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 09:17:13 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -78,7 +78,7 @@ const createApp = database => {
 
   const calculateOtherReduction = date => {
     let reduction = 0;
-    if (date && isPlainMonday(date) && isPlainHoliday(date)) {
+    if (date && isPlainMonday(date) && !isPlainHoliday(date)) {
       reduction = 35;
     }
     return reduction;
