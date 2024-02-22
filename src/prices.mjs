7b2a430ccf04f7a8df13ec039be2499a19a993c9
pacozavala <paco.zavala@houston-inc.com>
2024-02-22 09:20:43 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -107,9 +107,7 @@ const createApp = database => {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
-      if (
-        date
-      ) {
+      if (date) {
         return true;
       }
     }
